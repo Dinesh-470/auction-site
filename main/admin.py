18b84c2e14a820e39fe10faa9cb0a1b0661b8ecc
dinesh <dineshreddypaidi47@gmail.com>
2024-04-20 09:30:37 +0530

    added git ignore

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import users
 # Register your models here.
+class user_view(admin.AdminSite):
+    list_display = ['user_name','email','image']
+    
+admin.site.register(users)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    user_name = models.CharField(max_length=20)
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    number = models.CharField(max_length=15)
+    image = models.ImageField(upload_to='user_images/')
+    def __str__(self) -> str:
+        return self.user_name
+    
+        
